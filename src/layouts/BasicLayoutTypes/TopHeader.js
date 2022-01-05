f27534884905abfb0f39017eee60ce8c75d44774
windyzoe <windyzoe@sina.com>
2022-01-05 17:24:44 +0800

    去除粒子效果

--- src/layouts/BasicLayoutTypes/TopHeader.js
+++ src/layouts/BasicLayoutTypes/TopHeader.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useMount } from 'ahooks';
 import { Layout } from 'antd';
 import { DoubleLeftOutlined, DoubleRightOutlined } from '@ant-design/icons';
-import Particles from 'react-particles-js';
 import Logo from '@/components/Logo';
 import GlobalHeader from '@/components/GlobalHeader';
 import GlobalFooter from '@/components/GlobalFooter';
 import HorizenMenu from '@/components/GlobalMenu/HorizenMenu';
 import { getStore } from '@/app';
 import styles from '@/layouts/BasicLayout.less';
+import bgImg from '@/assets/menu.jpg';
 
 const { Header, Content, Footer, Sider } = Layout;
 /**
@@ -20,19 +20,9 @@ const { Header, Content, Footer, Sider } = Layout;
 export default function TopHeaderLayout({ children, collapsed, menuData }) {
   return (
     <Layout className={styles.topHeaderLayout}>
-      <Header
-        style={{ height: '6rem', padding: 0, lineHeight: '3rem', position: 'relative' }}
-        className={styles.header}
-      >
-        <Particles
-          height="6rem"
-          params={{ particles: { number: { value: 12 }, size: { value: 5 }, move: { speed: 1 } } }}
-          className={styles.particles}
-        />
-        <GlobalHeader
-          left={<Logo hiddenTitle={collapsed} />}
-          style={{ height: '3rem', color: '#fff', position: 'relative' }}
-        />
+      <Header style={{ height: '6rem', padding: 0, lineHeight: '3rem', position: 'relative' }} className={styles.header}>
+        <div className={styles.menuBgImg} style={{ backgroundImage: `url(${bgImg})` }} />
+        <GlobalHeader left={<Logo hiddenTitle={collapsed} />} style={{ height: '3rem', color: '#fff', position: 'relative' }} />
         <div className={styles.menu} style={{ height: '3rem' }}>
           <HorizenMenu menuData={menuData} collapsed={collapsed} />
         </div>
